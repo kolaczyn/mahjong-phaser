2dc7e22648f63311c68e58db3f4c53f85c8b18b6
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2024-10-28 21:35:57 +0100

    tiles setup

---
+++ src/tiles.ts
@@ -0,0 +1,189 @@
+import { DragonTile, SuitTile, WindTile } from "./types.ts";
+
+export const pinTiles: SuitTile[] = [
+  { id: "1-pin-1", kind: "pin", value: 1 },
+  { id: "1-pin-2", kind: "pin", value: 1 },
+  { id: "1-pin-3", kind: "pin", value: 1 },
+  { id: "1-pin-4", kind: "pin", value: 1 },
+
+  { id: "2-pin-1", kind: "pin", value: 2 },
+  { id: "2-pin-2", kind: "pin", value: 2 },
+  { id: "2-pin-3", kind: "pin", value: 2 },
+  { id: "2-pin-4", kind: "pin", value: 2 },
+
+  { id: "3-pin-1", kind: "pin", value: 3 },
+  { id: "3-pin-2", kind: "pin", value: 3 },
+  { id: "3-pin-3", kind: "pin", value: 3 },
+  { id: "3-pin-4", kind: "pin", value: 3 },
+
+  { id: "4-pin-1", kind: "pin", value: 4 },
+  { id: "4-pin-2", kind: "pin", value: 4 },
+  { id: "4-pin-3", kind: "pin", value: 4 },
+  { id: "4-pin-4", kind: "pin", value: 4 },
+
+  { id: "5-pin-1", kind: "pin", value: 5 },
+  { id: "5-pin-2", kind: "pin", value: 5 },
+  { id: "5-pin-3", kind: "pin", value: 5 },
+  { id: "5-pin-4", kind: "pin", value: 5 },
+
+  { id: "6-pin-1", kind: "pin", value: 6 },
+  { id: "6-pin-2", kind: "pin", value: 6 },
+  { id: "6-pin-3", kind: "pin", value: 6 },
+  { id: "6-pin-4", kind: "pin", value: 6 },
+
+  { id: "7-pin-1", kind: "pin", value: 7 },
+  { id: "7-pin-2", kind: "pin", value: 7 },
+  { id: "7-pin-3", kind: "pin", value: 7 },
+  { id: "7-pin-4", kind: "pin", value: 7 },
+
+  { id: "8-pin-1", kind: "pin", value: 8 },
+  { id: "8-pin-2", kind: "pin", value: 8 },
+  { id: "8-pin-3", kind: "pin", value: 8 },
+  { id: "8-pin-4", kind: "pin", value: 8 },
+
+  { id: "9-pin-1", kind: "pin", value: 9 },
+  { id: "9-pin-2", kind: "pin", value: 9 },
+  { id: "9-pin-3", kind: "pin", value: 9 },
+  { id: "9-pin-4", kind: "pin", value: 9 },
+];
+
+export const souTiles: SuitTile[] = [
+  { id: "1-sou-1", kind: "sou", value: 1 },
+  { id: "1-sou-2", kind: "sou", value: 1 },
+  { id: "1-sou-3", kind: "sou", value: 1 },
+  { id: "1-sou-4", kind: "sou", value: 1 },
+
+  { id: "2-sou-1", kind: "sou", value: 2 },
+  { id: "2-sou-2", kind: "sou", value: 2 },
+  { id: "2-sou-3", kind: "sou", value: 2 },
+  { id: "2-sou-4", kind: "sou", value: 2 },
+
+  { id: "3-sou-1", kind: "sou", value: 3 },
+  { id: "3-sou-2", kind: "sou", value: 3 },
+  { id: "3-sou-3", kind: "sou", value: 3 },
+  { id: "3-sou-4", kind: "sou", value: 3 },
+
+  { id: "4-sou-1", kind: "sou", value: 4 },
+  { id: "4-sou-2", kind: "sou", value: 4 },
+  { id: "4-sou-3", kind: "sou", value: 4 },
+  { id: "4-sou-4", kind: "sou", value: 4 },
+
+  { id: "5-sou-1", kind: "sou", value: 5 },
+  { id: "5-sou-2", kind: "sou", value: 5 },
+  { id: "5-sou-3", kind: "sou", value: 5 },
+  { id: "5-sou-4", kind: "sou", value: 5 },
+
+  { id: "6-sou-1", kind: "sou", value: 6 },
+  { id: "6-sou-2", kind: "sou", value: 6 },
+  { id: "6-sou-3", kind: "sou", value: 6 },
+  { id: "6-sou-4", kind: "sou", value: 6 },
+
+  { id: "7-sou-1", kind: "sou", value: 7 },
+  { id: "7-sou-2", kind: "sou", value: 7 },
+  { id: "7-sou-3", kind: "sou", value: 7 },
+  { id: "7-sou-4", kind: "sou", value: 7 },
+
+  { id: "8-sou-1", kind: "sou", value: 8 },
+  { id: "8-sou-2", kind: "sou", value: 8 },
+  { id: "8-sou-3", kind: "sou", value: 8 },
+  { id: "8-sou-4", kind: "sou", value: 8 },
+
+  { id: "9-sou-1", kind: "sou", value: 9 },
+  { id: "9-sou-2", kind: "sou", value: 9 },
+  { id: "9-sou-3", kind: "sou", value: 9 },
+  { id: "9-sou-4", kind: "sou", value: 9 },
+];
+
+export const manTiles: SuitTile[] = [
+  { id: "1-man-1", kind: "man", value: 1 },
+  { id: "1-man-2", kind: "man", value: 1 },
+  { id: "1-man-3", kind: "man", value: 1 },
+  { id: "1-man-4", kind: "man", value: 1 },
+
+  { id: "2-man-1", kind: "man", value: 2 },
+  { id: "2-man-2", kind: "man", value: 2 },
+  { id: "2-man-3", kind: "man", value: 2 },
+  { id: "2-man-4", kind: "man", value: 2 },
+
+  { id: "3-man-1", kind: "man", value: 3 },
+  { id: "3-man-2", kind: "man", value: 3 },
+  { id: "3-man-3", kind: "man", value: 3 },
+  { id: "3-man-4", kind: "man", value: 3 },
+
+  { id: "4-man-1", kind: "man", value: 4 },
+  { id: "4-man-2", kind: "man", value: 4 },
+  { id: "4-man-3", kind: "man", value: 4 },
+  { id: "4-man-4", kind: "man", value: 4 },
+
+  { id: "5-man-1", kind: "man", value: 5 },
+  { id: "5-man-2", kind: "man", value: 5 },
+  { id: "5-man-3", kind: "man", value: 5 },
+  { id: "5-man-4", kind: "man", value: 5 },
+
+  { id: "6-man-1", kind: "man", value: 6 },
+  { id: "6-man-2", kind: "man", value: 6 },
+  { id: "6-man-3", kind: "man", value: 6 },
+  { id: "6-man-4", kind: "man", value: 6 },
+
+  { id: "7-man-1", kind: "man", value: 7 },
+  { id: "7-man-2", kind: "man", value: 7 },
+  { id: "7-man-3", kind: "man", value: 7 },
+  { id: "7-man-4", kind: "man", value: 7 },
+
+  { id: "8-man-1", kind: "man", value: 8 },
+  { id: "8-man-2", kind: "man", value: 8 },
+  { id: "8-man-3", kind: "man", value: 8 },
+  { id: "8-man-4", kind: "man", value: 8 },
+
+  { id: "9-man-1", kind: "man", value: 9 },
+  { id: "9-man-2", kind: "man", value: 9 },
+  { id: "9-man-3", kind: "man", value: 9 },
+  { id: "9-man-4", kind: "man", value: 9 },
+];
+
+export const dragonTiles: DragonTile[] = [
+  { id: "white-dragon-1", kind: "dragon", value: "white" },
+  { id: "white-dragon-2", kind: "dragon", value: "white" },
+  { id: "white-dragon-3", kind: "dragon", value: "white" },
+  { id: "white-dragon-4", kind: "dragon", value: "white" },
+
+  { id: "green-dragon-1", kind: "dragon", value: "green" },
+  { id: "green-dragon-2", kind: "dragon", value: "green" },
+  { id: "green-dragon-3", kind: "dragon", value: "green" },
+  { id: "green-dragon-4", kind: "dragon", value: "green" },
+
+  { id: "red-dragon-1", kind: "dragon", value: "red" },
+  { id: "red-dragon-2", kind: "dragon", value: "red" },
+  { id: "red-dragon-3", kind: "dragon", value: "red" },
+  { id: "red-dragon-4", kind: "dragon", value: "red" },
+];
+
+export const windTiles: WindTile[] = [
+  { id: "east-wind-1", kind: "wind", value: "east" },
+  { id: "east-wind-2", kind: "wind", value: "east" },
+  { id: "east-wind-3", kind: "wind", value: "east" },
+  { id: "east-wind-4", kind: "wind", value: "east" },
+
+  { id: "south-wind-1", kind: "wind", value: "south" },
+  { id: "south-wind-2", kind: "wind", value: "south" },
+  { id: "south-wind-3", kind: "wind", value: "south" },
+  { id: "south-wind-4", kind: "wind", value: "south" },
+
+  { id: "west-wind-1", kind: "wind", value: "west" },
+  { id: "west-wind-2", kind: "wind", value: "west" },
+  { id: "west-wind-3", kind: "wind", value: "west" },
+  { id: "west-wind-4", kind: "wind", value: "west" },
+
+  { id: "north-wind-1", kind: "wind", value: "north" },
+  { id: "north-wind-2", kind: "wind", value: "north" },
+  { id: "north-wind-3", kind: "wind", value: "north" },
+  { id: "north-wind-4", kind: "wind", value: "north" },
+];
+
+export const allTiles = [
+  ...pinTiles,
+  ...souTiles,
+  ...manTiles,
+  ...dragonTiles,
+  ...windTiles,
+];
---
+++ src/types.ts
@@ -0,0 +1,41 @@
+type SuitKind = "pin" | "sou" | "man";
+
+type Value = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export type SuitTile = {
+  id: string;
+  kind: SuitKind;
+  value: Value;
+};
+
+type Wind = "east" | "south" | "west" | "north";
+
+export type WindTile = {
+  id: string;
+  kind: "wind";
+  value: Wind;
+};
+
+type Dragon = "white" | "green" | "red";
+
+export type DragonTile = {
+  id: string;
+  kind: "dragon";
+  value: Dragon;
+};
+
+export type Tile = SuitTile | WindTile | DragonTile;
+
+export type PlayerState = {
+  id: string;
+  hand: Tile[];
+  discards: Tile[];
+  wind: Wind;
+  points: number;
+};
+
+export type BoardState = {
+  players: PlayerState[];
+  wind: Wind;
+  wall: Tile[];
+};
